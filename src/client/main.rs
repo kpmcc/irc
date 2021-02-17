6f88bd600d9104b3dec1cdf6bc0e16786259875e
Kay Li <il@yakay.li>
2021-02-16 22:05:45 -0500

    fix

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,9 +1,9 @@
 use std::io::prelude::*;
-use std::net::{TcpStream};
+use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:3333")?;
-    stream.write(&[1])?;
-    stream.read(&mut [0; 128])?;
+    stream.write_all(&[1])?;
+    stream.read_exact(&mut [0; 128])?;
     Ok(())
 }
