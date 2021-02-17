aee1fa3d8c266dc10900ba356a30800fe9fb1dd3
Jefferson Airplane Guanciale <kldaace@gmail.com>
2021-02-16 22:17:16 -0500

    Fmt and fix

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,27 +1,23 @@
 use std::io::prelude::*;
-use std::net::{TcpStream};
+use std::net::TcpStream;
 use std::str;
 
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:3333")?;
-    let mut data = [0 as u8; 50];
-    let mut hello = "";
-    stream.write(&[1])?;
+    let mut data = [0_u8; 50];
+    stream.write_all(&[1])?;
     match stream.read(&mut data) {
-        Ok(size) => {
-            match str::from_utf8(&data) {
-                Ok(hello) => {
-                    println!("working {:?}", size); 
-                }
-                Err(_) => {
-                    println!("Error parsing data");
-                }
+        Ok(size) => match str::from_utf8(&data) {
+            Ok(_) => {
+                println!("working {:?}", size);
             }
-        }
+            Err(_) => {
+                println!("Error parsing data");
+            }
+        },
         Err(_) => {
             println!("Error reading");
         }
-
     };
     Ok(())
 }
