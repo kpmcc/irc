b01f5cd7423bf3800b535e9fa5b0bc59d050a12c
Kyle McCornack <kpmcc@fastmail.com>
2021-02-16 21:32:02 -0600

    client sends 'hi' and echos server response

--- src/client/main.rs
+++ src/client/main.rs
@@ -5,11 +5,11 @@ use std::str;
 fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:3333")?;
     let mut data = [0_u8; 50];
-    stream.write_all(&[1])?;
+    stream.write_all(&[104, 105])?;
     match stream.read(&mut data) {
-        Ok(size) => match str::from_utf8(&data) {
-            Ok(_) => {
-                println!("working {:?}", size);
+        Ok(_) => match str::from_utf8(&data) {
+            Ok(hello) => {
+                println!("Server says: {}", hello);
             }
             Err(_) => {
                 println!("Error parsing data");
