eb6e169f748e9e1cead3578e3cb2d676f07c66e9
Sam Donow <drsam94@gmail.com>
2021-03-09 22:26:38 -0500

    Add ServerState and Channel Map

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,137 +1,25 @@
 #[macro_use]
 extern crate bitflags;
 
-use std::collections::HashMap;
-use std::io::{Read, Write};
-use std::net::{Shutdown, TcpListener, TcpStream};
-use std::str;
-use std::sync::{Arc, Mutex};
-use std::thread;
-
+use std::net::TcpListener;
 mod channel;
 mod to_clrf_reader_writer;
 mod client;
 mod message;
-use crate::channel::build_channel;
-use crate::to_clrf_reader_writer::ToClrfReaderWriter;
-use crate::client::build_client;
-use crate::client::Client;
-use crate::message::parse_message;
-use crate::message::Message;
+mod serverstate;
 
-fn handle_message<T: Write>(
-    msg: &str,
-    stream: &mut T,
-    client_map: &mut HashMap<String, Client>,
-    nick_ref: &mut String,
-) {
-    let m = parse_message(msg);
-    println!("Message {:#?}", m);
-    stream.write_all(msg.as_bytes()).unwrap();
-
-    if let Message::Nickname(nick) = &m {
-        if !nick_ref.is_empty() {
-            println!("Changing nick from {} -> {}", nick_ref, nick.to_string());
-            if let Some(mut client) = client_map.remove(nick_ref) {
-                client.update_nick(nick.to_string());
-                client_map.insert(client.get_nick().to_string(), client);
-            } else {
-                println!("Existing nick not found?");
-            }
-        } else {
-            if client_map.contains_key(nick) {
-                println!("Nickname {} is already taken!", nick);
-                return;
-            }
-
-            let client = build_client(nick.to_string());
-
-            client_map.insert(client.get_nick().to_string(), client);
-        }
-
-        *nick_ref = nick.to_string();
-        println!("full client map");
-        for (key, value) in client_map {
-            println!("{} -> {}", key, value.get_nick());
-        }
-    }
-
-    if let Message::Join(channels, _) = m {
-        for _ in channels.iter() {
-            let channel = build_channel(["nick".to_string()].to_vec());
-            println!("Created channel {:#?}", channel);
-        }
-    }
-}
-
-fn handle_client(mut stream: &mut TcpStream, client_clone: Arc<Mutex<HashMap<String, Client>>>) -> Result<(), String> {
-    let mut data = [0_u8; 50]; // using 50 byte buffer
-    let mut nick = String::new();
-    let mut stream = ToClrfReaderWriter::new(&mut stream);
-    loop {
-        match stream.read(&mut data) {
-            Ok(size) => {
-                let mut client_data = client_clone.lock().unwrap();
-                if size == 0 {
-                    break;
-                }
-
-                // echo everything, returning error if decoding failed
-                match str::from_utf8(&data[0..size]) {
-                    Ok(d) => {
-                        handle_message(d, &mut stream, &mut *client_data, &mut nick);
-                    },
-                    Err(e) => {
-                        return Err(format!("{}", e));
-                    }
-                }
-            },
-            Err(e) => {
-                return Err(format!("{}", e));
-            }
-        }
-    }
-    if !nick.is_empty() {
-        // we are exiting so remove ourselves from client map
-        let mut client_data = client_clone.lock().unwrap();
-        client_data.remove(&nick);
-    }
-    Ok(())
-}
+use crate::serverstate::ServerState;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
     // accept connections and process them, spawning a new thread for each one
     println!("Server listening on port 3333");
 
-    let client_map: HashMap<String, Client> = HashMap::new();
-    let mutex = Arc::new(Mutex::new(client_map));
-
+    let server = ServerState::new();
     for stream in listener.incoming() {
-        let client_clone = Arc::clone(&mutex);
         match stream {
-            Ok(mut stream) => {
-                let peer_addr = stream.peer_addr().unwrap();
-                println!("New connection: {}", peer_addr);
-                thread::spawn(move || {
-                    // connection succeeded
-                    match handle_client(&mut stream, client_clone) {
-                        Ok(_) => {
-                            println!(
-                                "Terminating connection with {} without error",
-                                peer_addr,
-                            );
-                        },
-                        Err(e) => {
-                            println!(
-                                "An error occurred, terminating connection with {}: {}",
-                                peer_addr,
-                                e,
-                            );
-                        }
-                    }
-                    stream.shutdown(Shutdown::Both).unwrap();
-                });
+            Ok(stream) => {
+                server.handle_incoming_client(stream)
             }
             Err(e) => {
                 println!("Error: {}", e);
---
+++ src/server/serverstate.rs
@@ -0,0 +1,156 @@
+use std::sync::{Arc, Mutex};
+use std::net::{Shutdown, TcpStream};
+use std::thread;
+use std::collections::HashMap;
+use std::io::{Read, Write};
+use std::str;
+
+
+use crate::to_clrf_reader_writer::ToClrfReaderWriter;
+
+use crate::message::Message;
+use crate::message::parse_message;
+use crate::channel::Channel;
+use crate::channel::build_channel;
+use crate::client::Client;
+use crate::client::build_client;
+
+pub struct ServerState {
+    client_mutex : Arc<Mutex<HashMap<String, Client>>>,
+    channel_mutex : Arc<Mutex<HashMap<String, Channel>>>,
+}
+
+impl ServerState {
+    pub fn new() -> ServerState {
+        let client_map: HashMap<String, Client>  = HashMap::new();
+        let channel_map: HashMap<String, Channel> = HashMap::new();
+        let client_mutex: Arc<Mutex<HashMap<String, Client>>> = Arc::new(Mutex::new(client_map));
+        let channel_mutex: Arc<Mutex<HashMap<String, Channel>>> = Arc::new(Mutex::new(channel_map));
+        ServerState {
+            client_mutex,
+            channel_mutex
+        }
+    }
+
+    pub fn get_client_map(&self) -> Arc<Mutex<HashMap<String, Client>>> {
+        Arc::clone(&self.client_mutex)
+    }
+
+    pub fn get_channel_map(&self) -> Arc<Mutex<HashMap<String, Channel>>> {
+        Arc::clone(&self.channel_mutex)
+    }
+
+    fn handle_client(mut stream: TcpStream, client_clone: Arc<Mutex<HashMap<String, Client>>>, channel_clone: Arc<Mutex<HashMap<String, Channel>>>) -> Result<(), String>  {
+        let mut data = [0_u8; 50]; // using 50 byte buffer
+        let mut nick = String::new();
+        let mut stream = ToClrfReaderWriter::new(&mut stream)
+        loop {
+            match stream.read(&mut data) {
+                Ok(size) => {
+                    let mut client_data = client_clone.lock().unwrap();
+                    let mut channel_data = channel_clone.lock().unwrap();
+                    if size == 0 {
+                        break;
+                    }
+
+                    // echo everything!
+                    match str::from_utf8(&data[0..size]) {
+                        Ok(d) => {
+                            ServerState::handle_message(d, &stream, &mut *client_data, &mut *channel_data, &mut nick);
+                        }
+                        Err(e) => {
+                            return Err(format!("{}", e))
+                        }
+                    }
+                }
+                Err(e) => {
+                    return Err(format!("{}", e));
+                }
+            }
+        }
+        if !nick.is_empty() {
+            // we are exiting so remove ourselves from client map
+            let mut client_data = client_clone.lock().unwrap();
+            client_data.remove(&nick);
+        }
+        Ok(())
+    }
+
+    fn handle_message<T: Write>(
+        msg: &str,
+        mut stream: &T,
+        client_map: &mut HashMap<String, Client>,
+        channel_map: &mut HashMap<String, Channel>,
+        nick_ref: &mut String,
+    ) {
+        let m = parse_message(msg);
+        println!("Message {:#?}", m);
+        stream.write_all(msg.as_bytes()).unwrap();
+
+        if let Message::Nickname(nick) = &m {
+            if !nick_ref.is_empty() {
+                println!("Changing nick from {} -> {}", nick_ref, nick.to_string());
+                if let Some(mut client) = client_map.remove(nick_ref) {
+                    client.update_nick(nick.to_string());
+                    client_map.insert(client.get_nick().to_string(), client);
+                } else {
+                    println!("Existing nick not found?");
+                }
+            } else {
+                if client_map.contains_key(nick) {
+                    println!("Nickname {} is already taken!", nick);
+                    return;
+                }
+
+                let client = build_client(nick.to_string());
+                println!(
+                    "Creating client {} -> nick {} mode {}",
+                    stream.peer_addr().unwrap(),
+                    client.get_nick(),
+                    client.get_mode()
+                );
+
+                client_map.insert(client.get_nick().to_string(), client);
+            }
+
+            *nick_ref = nick.to_string();
+            println!("full client map");
+            for (key, value) in client_map {
+                println!("{} -> {}", key, value.get_nick());
+            }
+        }
+
+        if let Message::Join(channels, _) = m {
+            for chan_name in channels.iter() {
+                let channel = build_channel(["nick".to_string()].to_vec());
+                println!("Created channel {:#?}", channel);
+                channel_map.insert(chan_name.to_string(), channel);
+            }
+        }
+    }
+
+    pub fn handle_incoming_client(&self, stream: TcpStream) {
+        println!("New connection: {}", stream.peer_addr().unwrap());
+        let client_clone = self.get_client_map();
+        let channel_clone = self.get_channel_map();
+        thread::spawn(move || {
+            // connection succeeded
+            match ServerState::handle_client(stream, client_clone, channel_clone) {
+                Ok(_) => {
+                    println!(
+                        "Terminating connection with {} without error",
+                        peer_addr,
+                    );
+                },
+                Err(e) => {
+                    println!(
+                        "An error occurred, terminating connection with {}: {}",
+                        peer_addr,
+                        e,
+                    );
+                }
+            }
+            stream.shutdown(Shutdown::Both).unwrap();
+        });
+    }
+}
